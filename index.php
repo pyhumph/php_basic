f71cdc57b71b8895c0c61bb9b9ba7985e74c7bf2
humphrey <humphrey.david.dev@gmail.com>
2023-07-15 14:57:29 +0300

    intro to php

---
+++ index.php
@@ -0,0 +1,93 @@
+<!-- <?php
+  // echo "<b>Hello World!</b>";
+  // echo "<hr>";
+  // echo "<i>Hello World!</i>";
+  // echo "<hr>";
+  // echo "<u>Hello World!</u>";
+  // echo "<hr>";
+?> -->
+
+<!-- <p>
+  My name is 
+  <?//php// echo "Ramadhan Mabad!";
+  ?>
+</p> -->
+
+<!-- <p>
+  Now is <?php //echo date('d/m H:i:s'); ?>
+</p> -->
+
+<?php
+// This is date example 
+//echo date("y-m-d");
+
+#Thid is time example
+//echo date("h:i:s");
+
+/*
+This is a block of codes you want to comment
+
+echo "lorem ipsum";
+echo "lorem ipsum";
+echo "lorem ipsum";
+echo "lorem ipsum";
+echo "lorem ipsum";
+echo "lorem ipsum";
+
+*/
+
+// $txt = "Hello World!";
+// $x = 5;
+// $firstname = "Kelvin";
+// $lastname = "Mwadago";
+// $classname = "DICT";
+
+// echo "$firstname $lastname $classname";
+?>
+
+<!-- <?php
+  // $x = 5;
+  // $y = 4;
+  // $z = 9;
+  // $txt = "The sum of all numbers is";
+  // echo $x + $y + $z;
+?> -->
+
+<?php 
+// function add($x, $y)
+// {
+//   return $x + $y;
+// } 
+
+// function sub($x, $y)
+// {
+//   return $x - $y;
+// }
+
+// function mult($x, $y)
+// {
+//   return $x * $y;
+// }
+
+// function div($x, $y)
+// {
+//   return $x / $y;
+// }
+
+// echo add(10,2) . "<br>"; 
+// echo sub(10,2) . "<br>";
+// echo mult(10,2) . "<br>";
+// echo div(10,2) . "<br>";
+
+
+?>
+
+<?php
+function calc($wanafunzi)
+{
+  $magari = 6;
+  return $wanafunzi / $magari;
+}
+
+$wanafunzi =105;
+echo "Kila gari litabeba wanafunzi " .calc($wanafunzi);
